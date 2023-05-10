6b2a9cbfae4d89c274b72b4a329219c36fd34b19
Mark Albrand <mark.albrand5@gmail.com>
2023-05-10 12:39:09 -0600

    Storybook

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,4 +1,4 @@
-import { Button } from "./Button.jsx"
+import Button from "./Button"
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
