b8475326e03cc08b55f422914c7d6ca005f85274
Mark Albrand <mark.albrand5@gmail.com>
2023-05-10 12:50:41 -0600

    Aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: resolve(__dirname, "./src"),
+            },
+            {
+                find: "@components",
+                replacement: resolve(__dirname, "./src/components"),
+            },
+            {
+                find: "@pages",
+                replacement: resolve(__dirname, "./src/pages"),
+            },
+            {
+                find: "@hooks",
+                replacement: resolve(__dirname, "./src/hooks"),
+            },
+            {
+                find: "@components",
+                replacement: resolve(__dirname, "./src/components"),
+            },
+            {
+                find: "@store",
+                replacement: resolve(__dirname, "./src/store"),
+            },
+        ],
+    },
 })
