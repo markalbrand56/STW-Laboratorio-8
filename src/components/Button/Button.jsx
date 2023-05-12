59b743ae5adf59273ff31347e1d3c6e9f67fb970
Mark Albrand <mark.albrand5@gmail.com>
2023-05-12 00:53:19 -0600

    Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import styles from "./Button.module.css"
 import PropTypes from "prop-types"
+import styles from "./Button.module.css"
 
 function Button({ children, onClick, type = "primary" }) {
     return (
