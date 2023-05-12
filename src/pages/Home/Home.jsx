14b9fa586e495dcf2d08a1798a42e6d2384d28e8
Mark Albrand <mark.albrand5@gmail.com>
2023-05-12 00:59:34 -0600

    Button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,30 @@
 import React from "react"
-import HomeMenu from "../../components/HomeMenu/HomeMenu"
+import styles from "./Home.module.css"
+import Button from "../../components/Button/Button"
+import { navigate } from "../../store/index"
 
 function Home() {
-    return <HomeMenu />
+    const handleStartButtonClick = () => {
+        navigate("/game")
+    }
+
+    const handleConfigButtonClick = () => {
+        navigate("/config")
+    }
+
+    return (
+        <div className={styles.HomeMenu}>
+            <h1>Welcome</h1>
+            <div className={styles.Buttons}>
+                <Button type="primary" onClick={handleStartButtonClick}>
+                    Start
+                </Button>
+                <Button type="secondary" onClick={handleConfigButtonClick}>
+                    Settings
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
