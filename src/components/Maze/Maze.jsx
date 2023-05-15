ab8d32155cace7b195abaab3fb10c074aad87eb8
Mark Albrand <mark.albrand5@gmail.com>
2023-05-15 12:18:36 -0600

    Movimiento desde maze

--- src/components/Maze/Maze.jsx
+++ src/components/Maze/Maze.jsx
@@ -1,12 +1,68 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import Entity from "../Entity/Entity"
 import styles from "./Maze.module.css"
 
-function Maze({ json, width, height }){
-    console.log("dentro del maze")
+function Maze({ json, width, height }) {
+    const [playerPosition, setPlayerPosition] = useState({ x: 1, y: 1 })
     const containerWidth = parseInt(width, 10) * 2 + parseInt(width, 10) + 1
     const containerHeight = height * 2 + 1
+    const updateMaze = (jsonM, newPlayerPosition) => {
+        const newMaze = jsonM
+        newMaze[playerPosition.y][playerPosition.x] = " "
+        newMaze[newPlayerPosition.y][newPlayerPosition.x] = "p"
+        return newMaze
+    }
+
+    const handleKeyDown = (event) => {
+        const { key } = event
+        console.log("position", playerPosition)
+        console.log("dentro del handleKeyDown", key)
+        switch (key) {
+            case "ArrowUp":
+                if (json[playerPosition.y - 1][playerPosition.x] === " ") {
+                    updateMaze(json, { x: playerPosition.x, y: playerPosition.y - 1 })
+                    setPlayerPosition({
+                        ...playerPosition,
+                        y: playerPosition.y - 1,
+                    })
+                }
+                break
+            case "ArrowDown":
+                if (json[playerPosition.y + 1][playerPosition.x] === " ") {
+                    updateMaze(json, { x: playerPosition.x, y: playerPosition.y + 1 })
+                    setPlayerPosition({
+                        ...playerPosition,
+                        y: playerPosition.y + 1,
+                    })
+                }
+                break
+            case "ArrowLeft":
+                if (json[playerPosition.y][playerPosition.x - 1] === " ") {
+                    updateMaze(json, { x: playerPosition.x - 1, y: playerPosition.y })
+                    setPlayerPosition({
+                        ...playerPosition,
+                        x: playerPosition.x - 1,
+                    })
+                }
+                break
+            case "ArrowRight":
+                if (json[playerPosition.y][playerPosition.x + 1] === " ") {
+                    updateMaze(json, { x: playerPosition.x + 1, y: playerPosition.y })
+                    setPlayerPosition({
+                        ...playerPosition,
+                        x: playerPosition.x + 1,
+                    })
+                }
+                break
+            default:
+                break
+        }
+    }
+
+    useEffect(() => {
+        updateMaze(json, playerPosition)
+    }, [playerPosition])
 
     return (
         <div
@@ -15,6 +71,8 @@ function Maze({ json, width, height }){
                 gridTemplateColumns: `repeat(${containerWidth}, 50px)`,
                 gridTemplateRows: `repeat(${containerHeight}, 50px)`,
             }}
+            onKeyDown={handleKeyDown}
+            tabIndex="0"
         >
             {json.map((row) =>
                 row.map((col) => {
